67500587318ce10173df9cacc73f8360dbb2c29e
ailingangel <angel20081018@163.com>
2018-07-06 19:02:37 +0800

    get tags

--- src/install.js
+++ src/install.js
@@ -17,12 +17,34 @@ const GET_TEMPLATE = async function() {
                 reject(error || 'error');
             } else {
                 var data = JSON.parse(body).map((data) => data.name);
-                console.log(data);
                 resolve(data);
             }
         })
     });
     return templates;
+}
+
+const GET_TAGS = async function(template) {
+    var url = `https://api.github.com/repos/${GIT_INFO.orgName}/${template}/tags`;
+    console.log(url);
+    let tags = await new Promise((resolve, reject) => {
+        request({
+            method: 'GET',
+            uri: url,
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            resolveWithFullResponse: true
+        }, function(error, response, body) {
+            if (error || response.statusCode !== 200) {
+                reject(error || 'error');
+            } else {
+                var data = JSON.parse(body).map((data) => data.name);
+                resolve(data);
+            }
+        })
+    });
+    return tags;
 }
 
 const DOWNLOAD_REPO = async function() {
@@ -32,12 +54,22 @@ const DOWNLOAD_REPO = async function() {
 const INSTALL = async function() {
     let templates = await GET_TEMPLATE();
     let template = '';
-    console.log(template);
+    let version = '';
+    console.log(templates);
     prompt.start();
     prompt.get(['template'], function(err, result) {
         template = result.template;
         console.log(template);
+        let tags = await GET_TAGS(template);
+        console.log(tags);
+        prompt.get(['version'], function(err, result) {
+            version = result.version;
+            console.log(version);
+        });
     });
+
+
+
 
 }
 
